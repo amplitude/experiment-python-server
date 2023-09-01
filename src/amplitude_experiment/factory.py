b8defe43126d48e25e025f1262b0fd01dde19b7f
tyiuhc <137842098+tyiuhc@users.noreply.github.com>
2023-09-01 11:51:36 -0700

    fix: Allow creation of multiple client instances based on api key (#38)

--- src/amplitude_experiment/factory.py
+++ src/amplitude_experiment/factory.py
@@ -5,27 +5,25 @@ from .local.config import LocalEvaluationConfig
 
 remote_evaluation_instances = {}
 local_evaluation_instances = {}
-default_instance = '$default_instance'
 
 
 class Experiment:
-    """Provides factory methods for storing singleton instance of Client"""
+    """Provides factory methods for storing instances of Client"""
 
     @staticmethod
     def initialize_remote(api_key: str, config: RemoteEvaluationConfig = None) -> RemoteEvaluationClient:
         """
-        Initializes a singleton Client. This method returns a default singleton instance, subsequent calls to
-        init will return the initial instance regardless of input.
+        Initializes a remote evaluation client.
             Parameters:
                 api_key (str): The Amplitude API Key
                 config (RemoteEvaluationConfig): Optional Config
 
             Returns:
-                The remote evaluation client.
+                A remote evaluation client.
         """
-        if remote_evaluation_instances.get(default_instance) is None:
-            remote_evaluation_instances[default_instance] = RemoteEvaluationClient(api_key, config)
-        return remote_evaluation_instances[default_instance]
+        if remote_evaluation_instances.get(api_key) is None:
+            remote_evaluation_instances[api_key] = RemoteEvaluationClient(api_key, config)
+        return remote_evaluation_instances[api_key]
 
     @staticmethod
     def initialize_local(api_key: str, config: LocalEvaluationConfig = None) -> LocalEvaluationClient:
@@ -38,8 +36,8 @@ class Experiment:
                 config (RemoteEvaluationConfig): Optional Config
 
             Returns:
-                The local evaluation client.
+                A local evaluation client.
         """
-        if local_evaluation_instances.get(default_instance) is None:
-            local_evaluation_instances[default_instance] = LocalEvaluationClient(api_key, config)
-        return local_evaluation_instances[default_instance]
+        if local_evaluation_instances.get(api_key) is None:
+            local_evaluation_instances[api_key] = LocalEvaluationClient(api_key, config)
+        return local_evaluation_instances[api_key]
