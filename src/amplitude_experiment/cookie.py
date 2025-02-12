eadae62a41b757a9c07f130da211c99ad289f67f
Peter Zhu <7332407+zhukaihan@users.noreply.github.com>
2025-02-12 14:19:13 -0800

    feat: Add new cookie format (#61)
    * feat: Add new cookie format
    
    * Fix new format test

--- src/amplitude_experiment/cookie.py
+++ src/amplitude_experiment/cookie.py
@@ -1,3 +1,7 @@
+import json
+import logging
+import urllib.parse
+
 from .user import User
 import base64
 
@@ -6,29 +10,52 @@ class AmplitudeCookie:
     """This class provides utility functions for parsing and handling identity from Amplitude cookies."""
 
     @staticmethod
-    def cookie_name(api_key: str) -> str:
+    def cookie_name(api_key: str, new_format: bool = False) -> str:
         """
         Get the cookie name that Amplitude sets for the provided
             Parameters:
                 api_key (str): The Amplitude API Key
+                new_format (bool): True if cookie is in Browser SDK 2.0 format
 
             Returns:
                 The cookie name that Amplitude sets for the provided Amplitude API Key
        """
         if not api_key:
             raise ValueError("Invalid Amplitude API Key")
+
+        if new_format:
+            if len(api_key) < 10:
+                raise ValueError("Invalid Amplitude API Key")
+            return f"AMP_{api_key[0:10]}"
+
+        if len(api_key) < 6:
+            raise ValueError("Invalid Amplitude API Key")
         return f"amp_{api_key[0:6]}"
 
     @staticmethod
-    def parse(amplitude_cookie: str) -> User:
+    def parse(amplitude_cookie: str, new_format: bool = False) -> User:
         """
         Parse a cookie string and returns user
             Parameters:
                 amplitude_cookie (str):  A string from the amplitude cookie
+                new_format: True if cookie is in Browser SDK 2.0 format
 
             Returns:
                 Experiment User context containing a device_id and user_id (if available)
         """
+
+        if new_format:
+            decoding = base64.b64decode(amplitude_cookie).decode("utf-8")
+            try:
+                user_session = json.loads(urllib.parse.unquote_plus(decoding))
+                if "userId" not in user_session:
+                    return User(user_id=None, device_id=user_session["deviceId"])
+                return User(user_id=user_session["userId"], device_id=user_session["deviceId"])
+            except Exception as e:
+                logger = logging.getLogger("Amplitude")
+                logger.error("Error parsing the Amplitude cookie: " + str(e))
+                return User()
+
         values = amplitude_cookie.split('.')
         user_id = None
         if values[1]:
@@ -39,13 +66,24 @@ class AmplitudeCookie:
         return User(user_id=user_id, device_id=values[0])
 
     @staticmethod
-    def generate(device_id: str) -> str:
+    def generate(device_id: str, new_format: bool = False) -> str:
         """
         Generates a cookie string to set for the Amplitude Javascript SDK
             Parameters:
                 device_id (str):  A device id to set
+                new_format: True if cookie is in Browser SDK 2.0 format
 
             Returns:
                 A cookie string to set for the Amplitude Javascript SDK to read
         """
-        return f"{device_id}.........."
+        if not new_format:
+            return f"{device_id}.........."
+
+        user_session_hash = {
+            "deviceId": device_id
+        }
+        json_data = json.dumps(user_session_hash)
+        encoded_json = urllib.parse.quote(json_data)
+        base64_encoded = base64.b64encode(bytearray(encoded_json, "utf-8")).decode("utf-8")
+
+        return base64_encoded
