7f401dbbb44c6a0649f7054738a7e93e410e19f9
Peter Zhu <peter.zhu@amplitude.com>
2025-10-28 10:06:09 -0700

    fix: typing

--- src/amplitude_experiment/remote/fetch_options.py
+++ src/amplitude_experiment/remote/fetch_options.py
@@ -1,10 +1,11 @@
+from typing import Optional
 class FetchOptions:
-    def __init__(self, tracksAssignment: bool | None = None, tracksExposure: bool | None = None):
+    def __init__(self, tracksAssignment: Optional[bool] = None, tracksExposure: Optional[bool] = None):
         """
         Fetch Options
             Parameters:
-                tracksAssignment (bool | None): Whether to track the assignment. The default None means track the assignment event.
-                tracksExposure (bool | None): Whether to track the exposure. The default None means don't track the exposure event.
+                tracksAssignment (Optional[bool]): Whether to track the assignment. The default None means track the assignment event.
+                tracksExposure (Optional[bool]): Whether to track the exposure. The default None means don't track the exposure event.
         """
         self.tracksAssignment = tracksAssignment
         self.tracksExposure = tracksExposure
